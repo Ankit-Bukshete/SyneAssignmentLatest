c6e9f01720c76711a00b3cfdf1fc459477787ce5
Ankit-Bukshete <ankitbuk1986@gmail.com>
2023-06-21 00:06:28 +0530

    third commit for assignment

--- src/test/java/testrunners/MyTestRunner.java
+++ src/test/java/testrunners/MyTestRunner.java
@@ -1,15 +1,22 @@
 package testrunners;
 
 import org.junit.runner.RunWith;
-import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions( features = {"src/test/resources/features/AdminTest.feature"},
+@CucumberOptions(
+        features = {"src/test/resources/features/AdminTest.feature"},
         glue = {"stepdefinitions"},
-        plugin ={"pretty", "html:target/cucumber", "json:target/cucumber.json"})
+        plugin = {"pretty",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "timeline:test-output-thread/"
+
+
+        }
+
+)
 
 public class MyTestRunner {
 
