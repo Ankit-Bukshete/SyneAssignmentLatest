b073bb4602535e7f5ff989086fe7f312a93a9f53
Ankit-Bukshete <ankitbuk1986@gmail.com>
2023-06-20 22:35:50 +0530

    third commit for assignment

---
+++ test-output-thread/report.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "bc7aa7d5-add7-4752-82f5-75cb812ace3e",
+    "feature": "Admin Test Feature",
+    "scenario": "Login",
+    "start": 1687114300589,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1687114307678,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
