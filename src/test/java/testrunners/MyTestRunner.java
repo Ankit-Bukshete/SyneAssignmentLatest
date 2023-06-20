5bc2fb6a03b36296a3f85c55cf473ad1bd741848
Ankit-Bukshete <ankitbuk1986@gmail.com>
2023-06-20 23:39:46 +0530

    third commit for assignment

--- src/test/java/testrunners/MyTestRunner.java
+++ src/test/java/testrunners/MyTestRunner.java
@@ -7,7 +7,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions( plugin ={"pretty", "html:target/cucumber", "json:target/cucumber.json"})
+@CucumberOptions( features = {"src/test/resources/features/AdminTest.feature"},
+        glue = {"stepdefinitions"},
+        plugin ={"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 
 public class MyTestRunner {
 
